279c89c3d8c72d9e2534497a531e3bec0cd21f61
karthimech <35652925+karthimech@users.noreply.github.com>
2018-01-27 04:35:13 +0000

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,9 @@
+include<stdio.h>
+void main()
+{
+    char k=8;
+    if((k>='a' && k<='z')||(k>='A' && k<='Z'))
+    printf("\n it is an alphabet\n");
+    else
+    printf("\nit is an not alphabet");
+}
